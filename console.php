9addfe1189f4b93ee6f6f6710ce0005ff3d3934f
Georg Sieber <it@georg-sieber.de>
2022-08-26 23:29:57 +0200

    improve console.php error handling

--- console.php
+++ console.php
@@ -1,9 +1,9 @@
 <?php
-if(php_sapi_name() != "cli")
-	die("This script must be executed from command line.\n");
+if(php_sapi_name() != 'cli')
+	die('This script must be executed from command line.'."\n");
 
 if(!isset($argv[1]))
-	die("Please specify an action as first parameter (createuser|listuser|changeuserpasswd|deleteuser).\n");
+	die('Please specify an action as first parameter (createuser|listuser|changeuserpasswd|deleteuser).'."\n");
 
 require_once('lib/loader.php');
 
@@ -11,59 +11,80 @@ $null = null;
 switch($argv[1]) {
 
 	case 'createuser':
-		if(isset($argv[2]) && isset($argv[3])) {
-			if($db->insertClient($argv[2], password_hash($argv[3], PASSWORD_DEFAULT), $null) > 0) {
-				echo "createuser: ok\n";
-			} else {
-				echo "createuser: database operation error\n";
+		try {
+			if(!isset($argv[2]) || !isset($argv[3])) {
+				throw new Exception('missing arguments, need <username> <password>');
 			}
-		} else {
-			echo "createuser: missing arguments, need <username> <password>\n";
+			if($db->insertClient($argv[2], password_hash($argv[3], PASSWORD_DEFAULT), $null) > 0) {
+				echo $argv[1].': OK'."\n";
+			} else {
+				throw new Exception('database operation error');
+			}
+		} catch(Exception $e) {
+			echo $argv[1].' ERROR: '.$e->getMessage()."\n";
+			exit(1);
 		}
 		break;
 
 	case 'listuser':
-		$clients = $db->getClients();
-		if($clients !== null) {
-			foreach($clients as $client) {
-				echo $client->id." ".$client->email." ".$client->last_login."\n";
+		try {
+			$clients = $db->getClients();
+			if($clients !== null) {
+				foreach($clients as $client) {
+					echo $client->id." ".$client->email." ".$client->last_login."\n";
+				}
+				echo $argv[1].': OK'."\n";
+			} else {
+				throw new Exception('database operation error');
 			}
-			echo "listuser: ok\n";
-		} else {
-			echo "listuser: database operation error\n";
+		} catch(Exception $e) {
+			echo $argv[1].' ERROR: '.$e->getMessage()."\n";
+			exit(1);
 		}
 		break;
 
 	case 'changeuserpasswd':
-		if(isset($argv[2])) {
+		try {
+			if(!isset($argv[2])) {
+				throw new Exception('missing arguments, need <userid> <new-password>');
+			}
 			if($db->getClient(intval($argv[2])) !== null) {
 				if($db->setClientPassword(intval($argv[2]), password_hash($argv[3], PASSWORD_DEFAULT))) {
-					echo "changeuserpasswd: ok\n";
+					echo $argv[1].': OK'."\n";
 				} else {
-					echo "changeuserpasswd: database operation error\n";
+					throw new Exception('database operation error');
 				}
 			} else {
-				echo "changeuserpasswd: userid not found\n";
+				throw new Exception('userid not found');
 			}
-		} else {
-			echo "changeuserpasswd: missing arguments, need <userid> <new-password>\n";
+		} catch(Exception $e) {
+			echo $argv[1].' ERROR: '.$e->getMessage()."\n";
+			exit(1);
 		}
 		break;
 
 	case 'deleteuser':
-		if(isset($argv[2])) {
+		try {
+			if(!isset($argv[2])) {
+				throw new Exception('missing arguments, need <userid>');
+			}
 			if($db->getClient(intval($argv[2])) !== null) {
 				if($db->deleteClient(intval($argv[2]))) {
-					echo "deleteuser: ok\n";
+					echo $argv[1].': OK'."\n";
 				} else {
-					echo "deleteuser: database operation error\n";
+					throw new Exception('database operation error');
 				}
 			} else {
-				echo "deleteuser: userid not found\n";
+				throw new Exception('userid not found');
 			}
-		} else {
-			echo "deleteuser: missing arguments, need <userid>\n";
+		} catch(Exception $e) {
+			echo $argv[1].' ERROR: '.$e->getMessage()."\n";
+			exit(1);
 		}
 		break;
+
+	default:
+		echo $argv[1].' ERROR: unknown command'."\n";
+		exit(1);
 
 }
